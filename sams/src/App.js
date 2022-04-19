86919c2c483f575e0ef3ccffbd18f88f0894970f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2022-04-19 17:07:41 -0400

    Set-up

---
+++ sams/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <h1> Welcome to SAMS grocery!</h1>
+      <Header/>
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ sams/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBHCd1v6Df3Ood1PUpI23napPFhVm1BS7M",
+    authDomain: "sams-grocery.firebaseapp.com",
+    projectId: "sams-grocery",
+    storageBucket: "sams-grocery.appspot.com",
+    messagingSenderId: "763693325576",
+    appId: "1:763693325576:web:8fba9868ba294bbdc660bd",
+    measurementId: "G-E9JGY0EMX5"
+  };
