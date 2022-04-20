0afb83bc7f7756d1c5b44ea58e64bc37a4f25918
AnkitJagtap01 <jagtap.an@northeastern.edu>
2022-04-20 03:24:56 -0400

    Homepage enhancement #2 & Routing checkout page

--- sams/src/App.js
+++ sams/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <h1> Welcome to SAMS grocery!</h1>
-      <Header/>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM A CHECKOUT, SMASH THE LIKE BUTTON</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+        <h1> Welcome to SAMS grocery!</h1>
+      </div>
+    </Router>
   );
 }
 
--- sams/src/Home.js
+++ sams/src/Home.js
@@ -1,18 +1,75 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
     <div className="home">
       <div className="home_container">
         <img
-          className='home_image'
+          className="home_image"
           src="https://m.media-amazon.com/images/I/71+6X396igL._SX3740_.jpg"
           alt=""
         />
+        <div className="home_row">
+          <Product
+            title="Frito-Lay, (18 Pack)"
+            price={11.79}
+            image="https://images-na.ssl-images-amazon.com/images/I/81UUOj-aSJL.AC_SL240_.jpg"
+            rating={3}
+          />
+          <Product
+            title="Strawberries, 1 lb"
+            price={3.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/8143FLAy6GL.AC_SL240_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Chicken Patties, 26 Oz."
+            price={7.19}
+            image="https://m.media-amazon.com/images/I/81xPiL7ApkL._SX679_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Blueberry Waffle, 9 Oz"
+            price={3.49}
+            image="https://m.media-amazon.com/images/I/81OqrqQezaL._SX679_.jpg"
+            rating={4}
+          />
+        </div>
+
+        <div className="home_row">
+          <Product
+            title="Butter and Grape Jelly Sandwiches, 2 Ounces"
+            price={3.49}
+            image="https://m.media-amazon.com/images/I/91+DPhTIvfL._SX679_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Brown Eggs - 18 ct"
+            price={6.37}
+            image="https://m.media-amazon.com/images/I/61Opqp++pGL._SX679_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Rich Milk Chocolate, 8 Count"
+            price={6.99}
+            image="https://m.media-amazon.com/images/I/816fAOhb++L._SX679_PIbundle-8,TopRight,0,0_AA679SH20_.jpg"
+            rating={4}
+          />
+        </div>
+
+        <div className="home_row">
+          <Product
+            title="Hostess Cup Cakes, 8 Count"
+            price={4.19}
+            image="https://m.media-amazon.com/images/I/81nyvg6HV4L._SX679_PIbundle-8,TopRight,0,0_AA679SH20_.jpg"
+            rating={4}
+          />
+        </div>
       </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
