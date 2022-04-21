e1d9690596ce7421f24732d39bc13f78b8c51c1b
sahilhps <sonawane.sa@northeastern.edu>
2022-04-20 23:43:57 -0400

    Deletion of Items from Cart
    Removing items from Cart

--- sams/src/Subtotal.js
+++ sams/src/Subtotal.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
+    const [{ basket }, dispatch] = useStateValue();
   return (
     <div className='subtotal'>
     <CurrencyFormat 
@@ -10,7 +13,7 @@ function Subtotal() {
             <>
 
             <p>
-        Subtotal (0 items): <strong>0</strong>
+        Subtotal ({basket?.length}): <strong>{value}</strong>
             </p>
 
             <small className='subtotal__gift'>
@@ -21,7 +24,7 @@ function Subtotal() {
         )}
 
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
         prefix={"$"}
