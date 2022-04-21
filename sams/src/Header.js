43f11616548527bfc488dc14e7232038fe343ef1
sahilhps <sonawane.sa@northeastern.edu>
2022-04-20 20:33:05 -0400

    Working on Cart
    Till Decided Part 3

--- sams/src/Header.js
+++ sams/src/Header.js
@@ -2,15 +2,22 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="header">
+      <Link to="/">
       <img
         className="header_logo"
         src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt=""
       />
+      </Link>
+     
 
       <div className="header_search">
         <input className="header_searchInput" type="text" />
@@ -31,10 +38,12 @@ function Header() {
           <span className="header_optionLineTwo">Prime</span>
         </div>
 
+        <Link to="/checkout">
         <div className="header_optionBasket">
           <ShoppingBasketIcon />
-          <span className="header_optionLineTwoheader_basketCount">0</span>
+          <span className="header_optionLineTwoheader_basketCount">{basket?.length}</span>
         </div>
+        </Link>
       </div>
     </div>
   );
