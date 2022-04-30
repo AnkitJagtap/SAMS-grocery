1e2a884f6f355ec9997ccfbcd33f8da91d58a9bb
sahilhps <sonawane.sa@northeastern.edu>
2022-04-30 11:26:10 -0400

    Adding Corousal

--- sams/src/Checkout.js
+++ sams/src/Checkout.js
@@ -9,11 +9,56 @@ function Checkout() {
   return (
     <div className="checkout">
       <div className="checkout__left">
-        <img
+        {/* <img
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt="ad"
+        /> */}
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  {/* <!-- Indicators --> */}
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  {/* <!-- The slideshow --> */}
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     
+    <img
+          className="checkout__ad"
+          src="https://cdn.filestackcontent.com/Gh0EEz6BQhSRrJGpdSVH"
+          alt="ad"
         />
+    </div>
+    <div class="carousel-item">
+    <img
+          className="checkout__ad"
+          src="https://cdn.filestackcontent.com/JqpTr2kWT8OhWmqTtdep"
+          alt="ad"
+        />
+    </div>
+    <div class="carousel-item">
+    <img
+          className="checkout__ad"
+          src="https://cdn.filestackcontent.com/qlS7XURVQtih7loqWzoT"
+          alt="ad"
+        />
+    </div>
+  </div>
+  
+  {/* <!-- Left and right controls --> */}
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
 
         <div className="checkout__title">
           <h3>Hi, {user?.email}</h3>
