a18ab23fd89ec551cbc16786f67022c8c5189505
Pathuri Sai Saran <99058160+SaiSaranPathuriNEU@users.noreply.github.com>
2022-04-29 20:32:56 -0400

    Stripe payment gateway successful
    Stripe payment are done successfully

--- sams/src/axios.js
+++ sams/src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "...", // THE API (clould  function) URL
+  baseURL: "http://localhost:5001/sams-grocery-3aa2f/us-central1/api", // THE API (clould  function) URL
+  // baseURL: '...' // at this point we don't have an API, we've done no cloud-function work, noting like that. THE API (cloud function) URL
+  // baseURL: 'http://localhost:5001/sams-grocery-3aa2f/us-central1/api'  // it is /api because we have
+  // exports.api = functions.https.onRequest(app); in functions index.js
 });
 
 export default instance;
